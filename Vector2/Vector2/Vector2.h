131885f572a15e469c6d47f5e95ddf781603b26e
Jack Keogh <keeeogh@gmail.com>
2018-10-05 14:37:30 +0100

    Constructor Methods Added

--- Vector2/Vector2/Vector2.h
+++ Vector2/Vector2/Vector2.h
@@ -23,7 +23,34 @@ namespace jk
 		T x;
 		T y;
 
+		/// <summary>
+		/// Default constructor
+		/// 
+		/// Initialises x and y variables to 0.
+		/// </summary>
+		Vector2() :
+			x(0), y(0) {};
 
+		/// <summary>
+		/// Overload constructor
+		/// 
+		/// Initialises x and y to parameters
+		/// passed in.
+		/// </summary>
+		/// <param name="vx">Overload value for x.</param>
+		/// <param name="vy">Overload value for y.</param>
+		Vector2(T vx, T vy) :
+			x(vx), y(vy) {};
+
+		/// <summary>
+		/// Overload constructor
+		/// 
+		/// Initialises x and y to parameters
+		/// of Vector2 passed in.
+		/// </summary>
+		/// <param name="v">Vector2 to overload x and y with.</param>
+		Vector2(const Vector2& v) :
+			x(v.x), y(v.y) {};
 	};
 
 	typedef Vector2<int> Vector2I;
